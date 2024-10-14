113f224256a8ca98365b54fd506e3f787f1ea8f3
vikas rahangdale <vikasrahangdale5606@gmail.com>
2024-10-14 19:53:40 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+let arr = [
+    function (a ,b){
+  return (a+b)
+    },
+    function (a ,b){
+        return (a-b)
+          },
+          function (a ,b){
+            return (a*b)
+              },
+              function (a ,b){
+                return (a/b)
+                  }
+]
+
+let ans = arr[3]
+ let final = ans(10,2)
+console.log(final);
+
+// function se data structure alogorith code
